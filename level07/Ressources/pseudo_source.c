2da534a6606dafbf41a5df07c19e2c331dcdb2dc
Tactoe <eotcat@gmail.com>
2023-11-09 13:39:41 +0100

    7 source code cleanup

--- level07/Ressources/pseudo_source.c
+++ level07/Ressources/pseudo_source.c
@@ -106,13 +106,41 @@ int main(int ac,char **av,char **env)
   command[17] = '\0';
   command[18] = '\0';
   command[19] = '\0';
+//   08048791 8d 5c 24 24     LEA        EBX=>buffer,[ESP + 0x24]
+// 08048795 b8 00 00        MOV        EAX,0x0
+//          00 00
+// 0804879a ba 64 00        MOV        EDX,0x64
+//          00 00
+// 0804879f 89 df           MOV        EDI,EBX
+// 080487a1 89 d1           MOV        ECX,EDX
+// 080487a3 f3 ab           STOSD.REP  ES:EDI
+
+  // translated by ghidra as 
+
   // piVar5 = buffer;
   // for (iVar2 = 100; iVar2 != 0; iVar2 = iVar2 + -1) {
   //   *piVar5 = 0;
   //   piVar5 = piVar5 + 1;
   // }
+
+  // easier to say
   memset(buffer, 0, 100);
+
   for (; *avCopy != (char *)0x0; avCopy = avCopy + 1) {
+    // 080487a7 8b 44 24 1c     MOV        EAX,dword ptr [ESP + 0x1c]=>avCopy
+    // 080487ab 8b 00           MOV        EAX,dword ptr [EAX]
+    // 080487ad c7 44 24        MOV        dword ptr [ESP + 0x14]=>local_1cc,0xffffffff
+    //          14 ff ff 
+    //          ff ff
+    // 080487b5 89 c2           MOV        EDX,EAX
+    // 080487b7 b8 00 00        MOV        EAX,0x0
+    //          00 00
+    // 080487bc 8b 4c 24 14     MOV        ECX,dword ptr [ESP + 0x14]=>local_1cc
+    // 080487c0 89 d7           MOV        EDI,EDX
+    // 080487c2 f2 ae           SCASB.REP   ES:EDI
+
+    // translated by ghidra as
+
     // uVar3 = 0xffffffff;
     // pcVar6 = *avCopy;
     // do {
@@ -121,11 +149,28 @@ int main(int ac,char **av,char **env)
     //   cVar1 = *pcVar6;
     //   pcVar6 = pcVar6 + (uint)bVar11 * -2 + 1;
     // } while (cVar1 != '\0');
-    strlen avCopy;
-    memset(*avCopy,0,~uVar3 - 1);
+    // memset(*avCopy,0,~uVar3 - 1);
+
+    //easier to say 
+
     memset(*avCopy,0,strlen(avCopy));
   }
   for (; *envCopy != (char *)0x0; envCopy = envCopy + 1) {
+
+    // 080487f6 8b 44 24 18     MOV        EAX,dword ptr [ESP + 0x18]=>envCopy
+    // 080487fa 8b 00           MOV        EAX,dword ptr [EAX]
+    // 080487fc c7 44 24        MOV        dword ptr [ESP + 0x14]=>local_1cc,0xffffffff
+    //          14 ff ff 
+    //          ff ff
+    // 08048804 89 c2           MOV        EDX,EAX
+    // 08048806 b8 00 00        MOV        EAX,0x0
+    //          00 00
+    // 0804880b 8b 4c 24 14     MOV        ECX,dword ptr [ESP + 0x14]=>local_1cc
+    // 0804880f 89 d7           MOV        EDI,EDX
+    // 08048811 f2 ae           SCASB.RE   ES:EDI
+
+    // translated by ghidra as
+
     // uVar3 = 0xffffffff;
     // pcVar6 = *envCopy;
     // do {
@@ -134,17 +179,33 @@ int main(int ac,char **av,char **env)
     //   cVar1 = *pcVar6;
     //   pcVar6 = pcVar6 + (uint)bVar11 * -2 + 1;
     // } while (cVar1 != '\0');
-    strlen envCopy;
-    memset(*envCopy,0,~uVar3 - 1);
+    
+    //easier to say 
+
     memset(*envCopy,0,strlen(envCopy));
   }
   puts(
-      "----------------------------------------------------\n  Welcome to wil\'s crappy number stora ge service!   \n----------------------------------------------------\n Commands:                                          \n    store - store a number into the data storage    \n    read  - read a number from the data storage     \n    quit  - exit the program                        \n----------------------------------------------------\n   wil has reserved some storage :>                 \n----------------------------------------------------\n"
+      "----------------------------------------------------\n  Welcome to wil\'s crappy number storage service!   \n----------------------------------------------------\n Commands:                                          \n    store - store a number into the data storage    \n    read  - read a number from the data storage     \n    quit  - exit the program                        \n----------------------------------------------------\n   wil has reserved some storage :>                 \n----------------------------------------------------\n"
       );
   do {
     printf("Input command: ");
     i = 1;
     fgets(command,0x14,stdin);
+
+    //     08048887 8d 84 24        LEA        EAX=>command,[ESP + 0x1b8]
+    //          b8 01 00 00
+    // 0804888e c7 44 24        MOV        dword ptr [ESP + 0x14]=>local_1cc,0xffffffff
+    //          14 ff ff 
+    //          ff ff
+    // 08048896 89 c2           MOV        EDX,EAX
+    // 08048898 b8 00 00        MOV        EAX,0x0
+    //          00 00
+    // 0804889d 8b 4c 24 14     MOV        ECX,dword ptr [ESP + 0x14]=>local_1cc
+    // 080488a1 89 d7           MOV        EDI,EDX
+    // 080488a3 f2 ae           SCASB.RE   ES:EDI
+
+    // translated by ghidra as
+
     // uVar3 = 0xffffffff;
     // pcVar6 = command;
     // do {
@@ -153,11 +214,28 @@ int main(int ac,char **av,char **env)
     //   cVar1 = *pcVar6;
     //   pcVar6 = pcVar6 + (uint)bVar11 * -2 + 1;
     // } while (cVar1 != '\0');
-    strlen command;
-    uVar3 = ~uVar3;
-    bVar8 = uVar3 == 1;
-    bVar10 = uVar3 == 2;
-    command[uVar3 - 2] = '\0';
+    // uVar3 = ~uVar3;
+    // bVar8 = uVar3 == 1;
+    // bVar10 = uVar3 == 2;
+    // command[uVar3 - 2] = '\0';
+
+    //easier to say
+
+    command[strlen(command)] = '\0';
+
+    //     080488b7 8d 84 24        LEA        EAX=>command,[ESP + 0x1b8]
+    //          b8 01 00 00
+    // 080488be 89 c2           MOV        EDX,EAX
+    // 080488c0 b8 5b 8d        MOV        EAX,s_store_08048d5b                             = "store"
+    //          04 08
+    // 080488c5 b9 05 00        MOV        ECX,0x5
+    //          00 00
+    // 080488ca 89 d6           MOV        ESI,EDX
+    // 080488cc 89 c7           MOV        EDI,EAX
+    // 080488ce f3 a6           CMPSB.REPE ES:EDI=>s_store_08048d5b,ESI                     = "store"
+
+    // translated by ghidra as 
+
     // iVar2 = 5;
     // pbVar4 = (byte *)command;
     // pbVar7 = (byte *)"store";
@@ -169,13 +247,29 @@ int main(int ac,char **av,char **env)
     //   pbVar4 = pbVar4 + (uint)bVar11 * -2 + 1;
     //   pbVar7 = pbVar7 + (uint)bVar11 * -2 + 1;
     // } while (bVar10);
-    strncmp;
-    bVar9 = false;
-    bVar8 = (!bVar8 && !bVar10) == bVar8;
+    // bVar9 = false;
+    // bVar8 = (!bVar8 && !bVar10) == bVar8;
+
+    // easier to say
+    
+    bVar8 = strncmp(command, "store", 5);
     if (bVar8) {
       i = store_number(buffer);
     }
     else {
+      // 080488f8 8d 84 24        LEA        EAX=>command,[ESP + 0x1b8]
+      //          b8 01 00 00
+      // 080488ff 89 c2           MOV        EDX,EAX
+      // 08048901 b8 61 8d        MOV        EAX,s_read_08048d61                              = "read"
+      //          04 08
+      // 08048906 b9 04 00        MOV        ECX,0x4
+      //          00 00
+      // 0804890b 89 d6           MOV        ESI,EDX
+      // 0804890d 89 c7           MOV        EDI,EAX
+      // 0804890f f3 a6           CMPSB.REPE ES:EDI=>s_read_08048d61,ESI                      = "read"
+
+      // translated by ghidra as
+
       // iVar2 = 4;
       // pbVar4 = (byte *)command;
       // pbVar7 = (byte *)"read";
@@ -187,13 +281,31 @@ int main(int ac,char **av,char **env)
       //   pbVar4 = pbVar4 + (uint)bVar11 * -2 + 1;
       //   pbVar7 = pbVar7 + (uint)bVar11 * -2 + 1;
       // } while (bVar8);
-      strncmp;
-      bVar10 = false;
-      bVar8 = (!bVar9 && !bVar8) == bVar9;
+      // bVar10 = false;
+      // bVar8 = (!bVar9 && !bVar8) == bVar9;
+
+      // easier to say
+      
+      bVar8 = strncmp(command, "read", 4);
       if (bVar8) {
         i = read_number(buffer);
       }
       else {
+
+        // 08048939 8d 84 24        LEA        EAX=>command,[ESP + 0x1b8]
+        //          b8 01 00 00
+        // 08048940 89 c2           MOV        EDX,EAX
+        // 08048942 b8 66 8d        MOV        EAX,s_quit_08048d66                              = "quit"
+        //          04 08
+        // 08048947 b9 04 00        MOV        ECX,0x4
+        //          00 00
+        // 0804894c 89 d6           MOV        ESI,EDX
+        // 0804894e 89 c7           MOV        EDI,EAX
+        // 08048950 f3 a6           CMPSB.REPE ES:EDI=>s_quit_08048d66,ESI                      = "quit"
+
+
+        // translated by ghidra as 
+
         // iVar2 = 4;
         // pbVar4 = (byte *)command;
         // pbVar7 = (byte *)"quit";
@@ -205,8 +317,13 @@ int main(int ac,char **av,char **env)
         //   pbVar4 = pbVar4 + (uint)bVar11 * -2 + 1;
         //   pbVar7 = pbVar7 + (uint)bVar11 * -2 + 1;
         // } while (bVar8);
-        strncmp;
-        if ((!bVar10 && !bVar8) == bVar10) {
+        // if ((!bVar10 && !bVar8) == bVar10) {
+
+        // easier to say
+
+        bVar10 = strncmp(command, "quit", 4);
+
+        if (bVar10) {
           // canary check to see if the stack hasn't exploded, can be ignored in clean source
           if (local_14 == *(int *)(in_GS_OFFSET + 0x14)) {
             return 0;
